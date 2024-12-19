167ea7a672aa6b7ab52856377366d870f5229ed5
LongAtGitHub <98071520+LongAtGitHub@users.noreply.github.com>
2024-12-18 20:01:32 -0600

    done

--- helpers.py
+++ helpers.py
@@ -15,22 +15,12 @@ def create_train_test(X, y, split):
     y_train, y_test = train_test_split(y, train_size= split, random_state=42)
     return X_train, y_train, X_test, y_test 
 
-def check_stationarity(df):
-    '''Checks the stationarity of the time series data set. 
-    Used to determine if the data should be detrended.
-    df: Pandas dataframe that represents a time series dataset.
-    '''
-    result = adfuller(df, autolag='AIC') # Perform the Dickey-Fuller test
-    p_value = result[1]
-    print(f'ADF Statistic: {result[0]}')
-    print(f'p-value: {p_value}')
-    print('Stationary' if p_value < 0.05 else 'Non-Stationary')
 
 # ToDO finish this
 def calculate_metrics(y_test, y_pred): 
     '''Calculates the performance metrics for a prediction and prints them.
-        y_test: 
-        y_pred:
+        y_test: actual values
+        y_pred: predicted values
     '''
     mse = mean_squared_error(y_test, y_pred)
     rmse = np.sqrt(mse)
